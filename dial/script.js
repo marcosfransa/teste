23751efdbc5aad507927a77603d33f376b7014ce
marcosfransa <96637179+marcosfransa@users.noreply.github.com>
2023-05-05 05:44:51 -0300

    Add files via upload

---
+++ dial/script.js
@@ -0,0 +1,31 @@
+const bookmarks = [
+  {
+    name: "Google",
+    url: "https://www.google.com"
+  },
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com"
+  },
+  {
+    name: "YouTube",
+    url: "https://www.youtube.com"
+  },
+  {
+    name: "Twitter",
+    url: "https://www.twitter.com"
+  },
+  {
+    name: "GitHub",
+    url: "https://www.github.com"
+  }
+];
+
+const tilesContainer = document.querySelector("#tiles");
+
+bookmarks.forEach(bookmark => {
+  const tile = document.createElement("div");
+  tile.classList.add("tile");
+  tile.innerHTML = `<iframe src="${bookmark.url}" frameborder="0" allowfullscreen></iframe>`;
+  tilesContainer.appendChild(tile);
+});
